c131a898f2c16f58f0cf1d1f15abea867d894045
Miguel Castrejon <mcastre1>
2026-01-12 20:55:22 -0700

    Created and used a signin component for the login page, auth login is now working correctly.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,24 @@
 import { Stack } from "expo-router";
+import { AppState } from 'react-native';
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { supabase } from '../lib/supabase';
+
+// Tells Supabase Auth to continuously refresh the session automatically if
+// the app is in the foreground. When this is added, you will continue to receive
+// `onAuthStateChange` events with the `TOKEN_REFRESHED` or `SIGNED_OUT` event
+// if the user's session is terminated. This should only be registered once.
+AppState.addEventListener('change', (state) => {
+  if (state === 'active') {
+    supabase.auth.startAutoRefresh()
+  } else {
+    supabase.auth.stopAutoRefresh()
+  }
+})
 
 export default function RootLayout() {
- return <Stack>
-    <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-  </Stack>;
+  return (<SafeAreaProvider>
+    <Stack>
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+    </Stack>
+  </SafeAreaProvider>);
 }
