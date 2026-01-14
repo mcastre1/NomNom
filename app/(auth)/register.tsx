dd7333fbb76223d8a1741266bf450520f9eff93a
Miguel Castrejon <miguelcastrejongal@gmail.com>
2026-01-13 19:44:17 -0700

    Created the registration page, and correctly connected it to supabase auth.

--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -1,15 +1,16 @@
-import { Text, View } from "react-native";
+import SignUp from "@/components/signup";
+import { View } from "react-native";
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: "flex-start",
+        alignItems: "flex-start",
       }}
     >
-      <Text>Register</Text>
+      <SignUp/>
     </View>
   );
 }
