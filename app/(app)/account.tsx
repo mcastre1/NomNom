e49089cd00eea431fef36705cd815c78ca85549d
Miguel Castrejon <miguelcastrejongal@gmail.com>
2026-01-17 20:33:53 -0700

    Created a log out button on account page.

--- app/(app)/account.tsx
+++ app/(app)/account.tsx
@@ -1,19 +1,49 @@
 import { supabase } from "@/lib/supabase";
 import { router } from 'expo-router';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function Account() {
 
-    async function logOut(){
+    async function logOut() {
         await supabase.auth.signOut();
         router.replace("/(auth)/login");
     }
 
-    return (<View>
-        <Text>Hello</Text>
-        <TouchableOpacity  onPress={logOut}>
-            <Text >Log Out</Text>
-        </TouchableOpacity>
-
-    </View>)
+    return (
+        <View style={styles.container}>
+            <View style={styles.verticallySpaced}>
+                <TouchableOpacity style={styles.button} onPress={logOut}>
+                    <Text style={styles.buttonText}>Log Out</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 40,
+        padding: 12,
+        width: '100%',
+    },
+    verticallySpaced: {
+        paddingTop: 4,
+        paddingBottom: 4,
+        alignSelf: 'stretch',
+    },
+    mt20: {
+        marginTop: 20,
+    },
+    button: {
+        backgroundColor: "#801c1c",
+        padding: 14,
+        borderRadius: 8,
+        marginTop: 20,
+        alignItems: "center",
+    },
+    buttonText: {
+        color: "white",
+        fontWeight: "700",
+        fontSize: 16,
+    },
+})
