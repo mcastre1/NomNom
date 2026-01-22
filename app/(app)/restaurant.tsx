b047ae8b6302d8e0c64d8e1c895cd26575ea158b
Miguel Castrejon <mcastre1>
2026-01-21 19:05:47 -0700

    Aligned detailed restaurant information properly

--- app/(app)/restaurant.tsx
+++ app/(app)/restaurant.tsx
@@ -1,14 +1,33 @@
+import { Image } from 'expo-image';
 import { useLocalSearchParams } from 'expo-router';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+
+const PlaceholderImage = require('@/assets/images/adaptive-icon.png');
 
 export default function RestaurantScreen() {
-  const {name, address, genre } = useLocalSearchParams();
+  const {name, address, photoUrl, genre} = useLocalSearchParams();
 
   return (
-    <View>
+    <View style={styles.container}>
+      {photoUrl ? <Image style={styles.imageStyle} source={{uri: photoUrl}}/> :<Image style={styles.imageStyle} source={PlaceholderImage}/>}
       <Text>{name}</Text>
       <Text>{address}</Text>
       <Text>{genre}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    justifyContent:'flex-start',
+    alignItems:'center',
+    width:'100%',
+  },
+  imageStyle:{
+        width:120,
+        height:120,
+        borderRadius:10,
+        margin:20,
+    },
+})
