777cafb4434f269a502bbe47f024bf8e68f6a2cd
Miguel Castrejon <mcastre1>
2026-01-23 19:16:43 -0700

    able to pass textinput text back to modal calling screen.

--- app/(modals)/addDish.tsx
+++ app/(modals)/addDish.tsx
@@ -1,19 +1,30 @@
 import { resolveCallback } from "@/utils/modalCallback";
 import { router, useLocalSearchParams } from "expo-router";
-import { Button } from 'react-native';
+import { useState } from "react";
+import { Button, Text, TextInput, View } from 'react-native';
 
 export default function SelectDishModal() {
   const { callbackId } = useLocalSearchParams();
+  const [name, setName] = useState("");
+  const [rating, setRating] = useState(0);
+  const [note, setNote] = useState("");
 
   function submit() {
     resolveCallback(callbackId, {
-      dish: "Burrito",
+      dish: name,
       rating: 5,
-      notes: "Your wife loved it"
+      notes:  note,
     });
 
     router.dismiss();
   }
 
-  return <Button title="Save" onPress={submit} />;
+  return <View>
+    <Text>Name:</Text>
+    <TextInput onChangeText={setName}/>
+    <Text>Rating:</Text>
+    <Text>Note:</Text>
+    <TextInput onChangeText={setNote}/>
+    <Button title="Save" onPress={submit} />
+  </View>
 }
