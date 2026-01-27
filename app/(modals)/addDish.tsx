5d82bb004228c2d4b02124d4535af36f614033f5
Miguel Castrejon <mcastre1>
2026-01-26 19:30:59 -0700

    Working on addDish screen ui.

--- app/(modals)/addDish.tsx
+++ app/(modals)/addDish.tsx
@@ -1,7 +1,7 @@
 import { resolveCallback } from "@/utils/modalCallback";
 import { router, useLocalSearchParams } from "expo-router";
 import { useState } from "react";
-import { Button, Text, TextInput, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function SelectDishModal() {
   const { callbackId } = useLocalSearchParams();
@@ -19,12 +19,45 @@ export default function SelectDishModal() {
     router.dismiss();
   }
 
-  return <View>
-    <Text>Name:</Text>
-    <TextInput onChangeText={setName}/>
-    <Text>Rating:</Text>
-    <Text>Note:</Text>
-    <TextInput onChangeText={setNote}/>
+  return <View style={styles.container}>
+    <Text style={styles.label}>Name:</Text>
+    <TextInput style={styles.input} onChangeText={setName}/>
+    <Text style={styles.label}>Rating:</Text>
+    <Text style={styles.label}>Note:</Text>
+    <TextInput style={styles.input} onChangeText={setNote}/>
     <Button title="Save" onPress={submit} />
   </View>
 }
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 20,
+        gap: 12,
+        width: '100%',
+    },
+    label: {
+        fontSize: 16,
+        fontWeight: "600",
+        marginTop: 10,
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: "#ccc",
+        padding: 12,
+        borderRadius: 8,
+        fontSize: 16,
+    },
+    button: {
+        backgroundColor: "#4A90E2",
+        padding: 14,
+        borderRadius: 8,
+        marginTop: 20,
+        alignItems: "center",
+    },
+    buttonText: {
+        color: "white",
+        fontWeight: "700",
+        fontSize: 16,
+    },
+
+});
