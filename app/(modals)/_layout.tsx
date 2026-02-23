051913559850f823b575ace7979e4abe5f4b5887
Miguel Castrejon <miguelcastrejongal@gmail.com>
2026-02-22 21:09:39 -0700

    Added a custom star rating selector.

--- app/(modals)/_layout.tsx
+++ app/(modals)/_layout.tsx
@@ -2,6 +2,10 @@ import { Stack } from "expo-router";
 
 export default function ModalLayout() {
   return (
-    <Stack screenOptions={{ presentation: "modal" }} />
+    <Stack screenOptions={{
+      presentation: "modal",
+      headerStyle: { backgroundColor: 'black' },
+      headerTintColor: "white",
+    }} />
   );
 }
